85ab39afc534b6927d568e10b46d7fe8b8eea27a
Nikhil Pawar <nikhil10992@gmail.com>
2017-12-03 20:38:04 -0500

    added activity_main.xml

--- app/src/main/java/cse/a605/com/audio_locator/MainActivity.java
+++ app/src/main/java/cse/a605/com/audio_locator/MainActivity.java
@@ -6,25 +6,25 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.a605.cse.audio_locator.R;
 
 public class MainActivity extends AppCompatActivity {
+    public TextView textViewIp,textViewMsg;
+    Server server;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        textViewIp = (TextView) findViewById(R.id.text_view_ip);
+        textViewMsg = (TextView)findViewById(R.id.text_view_msg);
+        //Init the class
+        //Start the server
+        server = new Server(MainActivity.this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //Display the IP address of the server device
+        textViewIp.setText(server.getIpAddress());
     }
 }
