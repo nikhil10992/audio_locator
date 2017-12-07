df0c0f39b496eab44be130eeb10691c3de7cc45a
Siddharth Rajiv Shah <sshah26@buffalo.edu>
2017-12-06 22:31:51 -0500

    Forget to add onePlus ID to server file

--- app/src/main/java/cse/a605/com/audio_locator/Server.java
+++ app/src/main/java/cse/a605/com/audio_locator/Server.java
@@ -22,7 +22,7 @@ public class Server {
     private DirectionComputer directionComputer;
     ServerSocket serverSocket;
     static final int socketServerPORT = 8080;
-    static String[] ids = {"ce1a11bb6459491f","7d2195c0675bcd9f","bed43046e2258412","5756a1757d60c6ca"};
+    static String[] ids = {"bd54565b67801eed","7d2195c0675bcd9f","bed43046e2258412","5756a1757d60c6ca"};
     List<String> idsArr = Arrays.asList(ids);
     public Server(MainActivity mainActivity){
         this.mainActivity = mainActivity;
